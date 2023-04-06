cbd84d96b52d5b74501209203668e0174fffd716
laljidp <yuvalprajapati572@gmail.com>
2023-04-06 20:34:34 +0530

    fix: design of image place holder on chat.

--- components/ImagePlaceholder.js
+++ components/ImagePlaceholder.js
@@ -4,15 +4,9 @@ export default function ImagePlaceholder({ files = [] }) {
   if (files.length === 0) return null
 
   return (
-    <Box
-      display={'flex'}
-      gap={5}
-      padding={2}
-      bg={'cyan.100'}
-      borderRadius={'4'}
-    >
+    <Box display={'flex'} gap={2} padding={2} borderRadius={'4'}>
       {files.map((src, index) => (
-        <Box>
+        <Box bg="gray.300" borderRadius={'4px'} padding={1}>
           <Image width={75} height={75} src={src} alt="images" key={index} />
         </Box>
       ))}
