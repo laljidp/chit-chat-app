52a729e91157fe138e7bac791c6f90fbe5997f19
laljidp <yuvalprajapati572@gmail.com>
2023-04-10 18:02:22 +0530

    fix: condition for rendering joined rooms.

--- components/RoomList.js
+++ components/RoomList.js
@@ -3,7 +3,7 @@ import { Alert, Box, Button, Grid, Text } from '@chakra-ui/react'
 export default function RoomLists({ rooms = [], onRoomClick = () => {} }) {
   return (
     <Grid>
-      {rooms.length !== 0 && (
+      {rooms.length === 0 && (
         <Text
           color={'#fff'}
           fontWeight={600}
@@ -13,29 +13,28 @@ export default function RoomLists({ rooms = [], onRoomClick = () => {} }) {
           No rooms available ! 🥺
         </Text>
       )}
-      {false &&
-        rooms.map((room) => (
-          <Box
-            padding={'8px 15px'}
-            bg="facebook.300"
-            color={'#fff'}
-            fontWeight={500}
-            borderRadius={'4px'}
-            display={'flex'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            marginTop={1}
+      {rooms.map((room) => (
+        <Box
+          padding={'8px 15px'}
+          bg="facebook.300"
+          color={'#fff'}
+          fontWeight={500}
+          borderRadius={'4px'}
+          display={'flex'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          marginTop={1}
+        >
+          <Text>{room.title}</Text>
+          <Button
+            size="sm"
+            colorScheme="telegram"
+            onClick={() => onRoomClick(room)}
           >
-            <Text>{room.title}</Text>
-            <Button
-              size="sm"
-              colorScheme="telegram"
-              onClick={() => onRoomClick(room)}
-            >
-              Join
-            </Button>
-          </Box>
-        ))}
+            Join
+          </Button>
+        </Box>
+      ))}
     </Grid>
   )
 }
